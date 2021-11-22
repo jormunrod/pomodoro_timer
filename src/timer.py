ee2d997dd15391008d5eaca06aa7fc5fe4f13d08
Jorge Muñoz Rodríguez <jormunrod@alum.us.es>
2021-11-22 19:18:34 +0100

    First edition

---
+++ src/timer.py
@@ -0,0 +1,27 @@
+'''
+Created on 22 nov 2021
+
+@author: Jorge Muñoz
+'''
+import time
+
+print('¡Pomodoro empieza ahora!')
+for i in range(4):
+    t = 25*60
+    while t:
+        mins = t//60
+        secs = t%60
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(''+timer, end='\r')
+        time.sleep(1)
+        t -= 1
+print('¡Tiempo de descanso!')
+t = 5*60
+while t:
+    mins = t//60
+    secs = t%60
+    timer = '{:02d}:{:02d}'.format(mins, secs)
+    print(''+timer, end='\r')
+    time.sleep(1)
+    t -= 1
+print('¡Hora de trabajar!')
